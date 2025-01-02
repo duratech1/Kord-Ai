91c3ecbb9b9345ef0ebc4f07691a25157a373ae4
duratech1 <ernestoduro14321@gmail.com>
2025-01-02 16:24:01 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2349067339193", // your number with country code
-  OWNER_NAME: 'Miracle', // your owner name 
+  OWNER_NUMBERS: "233248716476", // your number with country code
+  OWNER_NAME: 'Durable', // your owner name 
   BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: '', // put your session id between the ''
+  SESSION_ID: 'kord_ai-wQwCfekueIqu', // put your session id between the ''
   INVINCIBLE_MODE: false, // toogle invincible mode 
   WORK_MODE: "Public",
   TIME_ZONE: "Africa/Lagos",
